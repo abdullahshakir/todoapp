f65d939515968e41a8cc6fa90e214c36c4d68541
knackinteractive <43797439+knackinteractive@users.noreply.github.com>
2019-03-22 15:08:07 +0500

    controller

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,3 +1,17 @@
 module.exports = function(app){
+    //view
+    app.get('/todo',function(req,res){
+        
+    });
+    //create
+    app.post('/todo',function(req,res){
 
+    });
+    //delete
+    app.delete('/todo',function(req,res){
+
+    });
+    app.put('/todo',function(req,res){
+
+    });
 };
