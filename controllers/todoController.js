e46c41737e36407daf531127ea7fc0442d99d5ad
knackinteractive <43797439+knackinteractive@users.noreply.github.com>
2019-03-22 16:13:36 +0500

    todo controller post and delete updated

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,15 +1,26 @@
+var bodyParser = require('body-parser');
+
+var data = [{item:'do html'},{item:'do css'},{item:'do javascript'},{item:'do bootstrap'}];
+
+var urlencodedParser = bodyParser.urlencoded({extended:false});
+
 module.exports = function(app){
     //view
     app.get('/todo',function(req,res){
-        res.render('todo');
+        res.render('todo',{todos:data});
     });
     //create
-    app.post('/todo',function(req,res){
-
+    app.post('/todo',urlencodedParser,function(req,res){
+        data.push(req.body);
+        res.json(data);
     });
     //delete
-    app.delete('/todo',function(req,res){
-
+    app.delete('/todo:item',function(req,res){
+        var item = req.params.item;
+        data = data.filter(function(todo){
+            return todo.item.replace(/ /g, '-') !== req.params.item;
+            //filters item on base of this return true or false if true it will be kept in filtered array
+        });
     });
     app.put('/todo',function(req,res){
 
