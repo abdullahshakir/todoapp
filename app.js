73a1747a0e24890e87368725aab5bb411a33181c
knackinteractive <43797439+knackinteractive@users.noreply.github.com>
2019-03-22 15:07:28 +0500

    adding some controller,view and appjs

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var todoController = require('./controllers/todoController');
+
+var app = express();
+
+//set up template engine
+
+app.set('view engine','ejs');
+
+//static files
+app.use('/assets',express.static('./public'));
+
+//fire controllers
+todoController(app);
+
+//listen to a port
+app.listen(3000);
+
+console.log("Server is started listening to port 3000");
